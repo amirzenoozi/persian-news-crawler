130caa69f8bcd30881356ce093ce4bab2ac88142
amirzenoozi <amirzenoozi72@gmail.com>
2023-06-21 17:35:29 +0330

    ⚡️ Add New Func For Archive Pages

--- scripts/database.py
+++ scripts/database.py
@@ -32,3 +32,33 @@ def insert_news_in_database(db_file, title, short_link, service, subgroup, abstr
     db_connection.close()
 
     return news
+
+def insert_news_in_archive(db_file, title, short_link, service, subgroup, abstract, body, tags, published_datetime):
+    """
+    Insert NEWS
+
+    Args:
+        db_file (str): Path to Database File
+        title (str): News Title
+
+    Returns:
+        int: NEWS ID
+    """
+    db_connection = sqlite3.connect(db_file)
+    db_cursor = db_connection.cursor()
+    # Check News is Exist Or Not
+    find_news_query = """SELECT id FROM news WHERE short_link = (?) LIMIT 1"""
+    news = db_cursor.execute(find_news_query, (short_link,)).fetchone()
+    # Insert New News
+    if not news:
+        try:
+            add_news_query = """INSERT INTO news (title, short_link, service, subgroup, abstract, body, tags, published_datetime) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+            db_cursor.execute(add_news_query, (title, short_link, service, subgroup, abstract, body, tags, published_datetime))
+        except sqlite3.Error as error:
+            print(error)
+
+    # Close DB Connection
+    db_connection.commit()
+    db_connection.close()
+
+    return news
